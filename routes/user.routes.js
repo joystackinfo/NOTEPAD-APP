e5f61f37dde71db87c8c5088ee3bfc18db0a29c0
joystackinfo <joynelsoninfo@gmail.com>
2025-11-05 02:58:55 +0100

    added user route

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const {registerUser} = require('../controllers/user.controller'); //import function fro controller
 const router = express.Router();
+
 
 router.get('/', (req, res) => {
   res.send('👋 User route working fine!');
 });
+
+
+// Register user
+router.post('/register', registerUser);
+
 
 module.exports = router;
