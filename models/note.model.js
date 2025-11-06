008563f889e4246ab3785207070bd0994d3c0d48
joystackinfo <joynelsoninfo@gmail.com>
2025-11-06 21:27:23 +0100

    edited server.js

--- models/note.model.js
+++ models/note.model.js
@@ -1,6 +1,21 @@
 const mongoose = require('mongoose'); // include monogoose
-const noteSchema = new mongoose.Schema({ // create note schema 
+const NoteSchema = new mongoose.Schema({ // create note schema 
 
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User", // links it to user model
+        required: true
+    },  // links each note to a user
+
+    content: {
+        type: string,
+        required: true
+    }, //stores the note text
+
+      CreatedAt:{
+        type: Date,
+        default: Date.now
+      }
 
 
 
@@ -21,7 +36,7 @@ const noteSchema = new mongoose.Schema({ // create note schema
 
 })
 
-const note = mongoose.model('Note', userSchema);
+const note = mongoose.model('Note', NoteSchema);
 
 
 module.exports = Note;
--- server.js
+++ server.js
@@ -1,20 +1,30 @@
-const express = require('express');
+const express = require('express')
+const dotenv = require("dotenv");
+const cors = require("cors"); 
 const mongoose = require('mongoose');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const app = express();
-const verifyToken = require('../middleware/authmiddleware.js');
-const userRoutes = require('./routes/user.routes');
-dotenv.config();
+ const userRoutes = require('./routes/user.route.js');
+const app = express()
+const verifyToken = require('./middleware/authMiddleware.js');
+const productRoutes = require('./routes/product.route.js');
+require('dotenv').config();
+
+
 
 
 //MIDDLEWARE
-app.use(cors());
-app.use(express.json());
-app.use(express.static('public'));
+app.use(express.json()) // Allow express to parse json
+app.use(express.urlencoded({extended:false})) // Allow express to parse urlencoded data
+
 
 // ROUTES
-app.use('/api/users' , userRoutes);
+// Routes
+const userRoutes = require("./routes/userRoutes");
+const noteRoutes = require("./routes/noteRoutes");
+
+app.use("/api/notes", noteRoutes);
+app.use("/api/users", userRoutes); //for user routes
+
+
 
 
   //FOR LOGGING TO TRACK REQUESTS
@@ -22,13 +32,17 @@ app.use((req, res, next) => {
   console.log(`${req.method} ${req.url}`);
   next();
 });
- 
 
 
 // Test route
 app.get('/', (req, res) => {  // create a get route
-  res.send('Hello, Welcome to the CRUD API server '); // send a response
+  res.send('Hello, Welcome to the  NOTEPAD server '); // send a response
 });
+
+
+
+
+
 
 
 // connect to mongodb database using mongoose
@@ -45,5 +59,3 @@ mongoose.connect(process.env.MONGODB_URI)
   .catch(err => {
     console.error(' Connection failed', err);
   });
-
-
